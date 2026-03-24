df956c506faa364ed6a8ba4119073ffafc50bc9e
Lokesh Nalla <nallalokesh23@gmail.com>
2026-03-24 16:09:15 +0530

    update node backend url to gitam-chatbot-2

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://gitam-chatbot-1.onrender.com/api',
+    baseURL: 'https://gitam-chatbot-2.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
